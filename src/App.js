1fbc79a14d70c92425ca718c573663462c2b78d0
Ihor849 <333marchelo333@gmail.com>
2023-03-28 22:48:06 +0300

    заняття1

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import Painting from './components/Painting';
+// import Painting from './components/Painting';
+import PaintingList from './components/PaintingList';
+import Section from './components/Section';
 import paintings from './paintings.json';
+
+// export default function App() {
+//   return (
+//     <div>
+//       {paintings.map(painting => (
+//         <Painting
+//           key={painting.id}
+//           imageUrl={painting.url}
+//           title={painting.title}
+//           author={painting.author.tag}
+//           profileUrl={painting.author.url}
+//           price={painting.price}
+//           quantity={painting.quantity}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
+
 export default function App() {
   return (
     <div>
-      <Painting
-        url={paintings[0].url}
-        title={paintings[0].title}
-        authorName={paintings[0].author.tag}
-        profileUrl={paintings[0].author.url}
-        price={paintings[0].price}
-      />
+      {/* <PaintingList items={paintings} /> */}
+      <Section title="Топ недели">
+        <PaintingList items={paintings} />
+      </Section>
 
-      <Painting
-        url={paintings[1].url}
-        title={paintings[1].title}
-        authorName={paintings[1].author.tag}
-        profileUrl={paintings[1].author.url}
-        price={paintings[1].price}
-      />
-
-      <Painting
-        url={paintings[2].url}
-        title={paintings[2].title}
-        authorName={paintings[2].author.tag}
-        profileUrl={paintings[2].author.url}
-        price={paintings[2].price}
-      />
+      <Section title="Лучшее">
+        <PaintingList items={paintings} />
+      </Section>
     </div>
   );
 }
--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,18 +1,39 @@
-export default function Painting({ url, title, profileUrl, author, price }) {
+import PropTypes from 'prop-types';
+import defaultImage from './default.jpg';
+
+Painting.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  profileUrl: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  qoantity: PropTypes.number.isRequired,
+};
+
+export default function Painting({
+  imageUrl = defaultImage,
+  title,
+  profileUrl,
+  author = 'не известен',
+  price,
+  quantity,
+}) {
   return (
+    //   const { url,title,profileUrl,author = 'не известен',price,} = props деструкторизация
     <div>
-      <img src={url} alt={title} width="480" />
+      <img src={imageUrl} alt={title} width="480" />
       <h2>{title}</h2>
       <p>
         Автор: <a href={profileUrl}>{author}</a>
       </p>
       <p>Цена: {price} кредитов</p>
-      <p>Доступность: заканчиваеться или есть в наличии</p>
+      <p>Доступность: {quantity < 10 ? 'заканчиваеться' : 'есть в наличии'}</p>
       <button type="button">Добавить в корзину</button>
     </div>
   );
 }
 
+// Стрелочная функция
 // const Painting = ({ url, title, profileUrl, author, price }) => {
 //   return (
 //     <div>
@@ -26,6 +47,6 @@ export default function Painting({ url, title, profileUrl, author, price }) {
 //       <button type="button">Добавить в корзину</button>
 //     </div>
 //   );
-// }
+// };
 
 // export default Painting;
